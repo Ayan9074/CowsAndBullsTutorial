06ebcd653f9037a2c354c52030a5dc9d95f614db
Ayan9074 <57524544+Ayan9074@users.noreply.github.com>
2020-11-24 16:18:39 +0400

    Uploading V1 of code

---
+++ PythonGCSETask.py
@@ -0,0 +1,48 @@
+import random
+
+print("Welcome to Bulls and Cows \nPress any key to begin")
+input("")
+
+usernumber = 0
+userguesses = 0
+
+
+number = random.randint(1000,9999)
+print("The number is {0} for debugging".format(number))
+def checknumber(userinput):
+    digits = [int(x) for x in str(number)]
+    digits2 = [int(x) for x in str(userinput)]
+    Bulls = 0
+    Cows = 0
+
+    for i in digits:
+        for ii in digits2:
+            if i == ii:
+                if digits.index(i) == digits2.index(ii):
+                    Bulls += 1
+                else:
+                    Cows += 1
+                digits2[digits2.index(ii)] = "extra"
+                digits[digits.index(i)] = "extra1"
+                    
+    print("There is {0} bull/s found and {1} cow/s found".format(Bulls, Cows))
+
+print("A random 4 digit number has been created, your job is to guess it")
+print("If you give up type exit")
+
+
+while usernumber != number:
+    print("Enter your number:")
+    usernumber= str(input(""))
+    userguesses += 1
+    if usernumber == 'exit':
+        break
+    usernumber = int(usernumber)
+    checknumber(int(usernumber))
+
+if usernumber == number:
+    print("The number is {0}".format(number))
+    print("Well done you have finished the game")
+else:
+    print("The number is {0}".format(number))
+    print("Better Luck Next Time")
